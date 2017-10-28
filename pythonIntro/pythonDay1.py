60dd1d8e890b68aeba144ac8c9c01676eda6f88b
HARSHIT <harshit9797@gmail.com>
2017-10-28 17:46:03 +0530

    My Django Girls app, first commit

---
+++ pythonIntro/pythonDay1.py
@@ -0,0 +1,14 @@
+name = 'Atul'
+
+
+def whoareyou(name):
+    print('Hi '+name)
+
+
+list = ['Monica', 'Lagartha', 'Sabrina', 'Sita']
+
+
+for name in list:
+    whoareyou(name)
+for i in range(1, 6):
+    print(i)
